07459dccc5ec2d176a588183c9324cd698ed7123
Juan <juanpmad@outlook.com>
2022-12-06 15:45:49 -0300

    script

--- script/script.js
+++ script/script.js
@@ -5,17 +5,17 @@ async function consulta(){
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': '428ed3a953msh02d5768a4e04aecp18be4ejsn2ad602cc0f35',
-            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+            'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
         }
     };
 
     let pesquisa = document.querySelector("#pesquisa").value;
-    let resposta = await fetch('https://spotify23.p.rapidapi.com/search/?q='+pesquisa +'&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+    let resposta = await fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q='+pesquisa, options)
     .then(response => response.json())
     .catch(err => console.error(err));
     console.log(resposta);
 
-    let imagemData = await fetch(resposta.albums.items[0].data.coverArt.sources[2].url);
+    let imagemData = await fetch(resposta.data[0].album.cover_xl);
     let imagem = await imagemData.blob();
     const imageObjectURL = URL.createObjectURL(imagem);
     let img = document.createElement('img');
@@ -23,9 +23,11 @@ async function consulta(){
     img.src = imageObjectURL;
     resultados.appendChild(img);
 
-    let infoData = await fetch(resposta.albums.items[0].data.name);
-    let info = await infoData.text();
-    /*info = info + " | " + await fetch(resposta.albums.items[0].data.artists.items[0].profile.name);
-    info = info + " (" + await fetch(resposta.albums.items[0].data.date.year) + ")"*/;
+    let info = resposta.data[0].artist.name;
+    info = info + " - " +resposta.data[0].album.title;
     document.querySelector(".infoTexto").innerHTML = info;
+
+    while(document.querySelectorAll('.img').length !== 1){
+        document.querySelector(".img").remove();
+    }
 }
