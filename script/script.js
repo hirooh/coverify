8f1868d56617491307fc48509fbc5bd2825518e0
Juan <juanpmad@outlook.com>
2022-12-06 17:09:38 -0300

    covereezer

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 document.querySelector('#enviar').addEventListener('click', consulta);
 
 async function consulta(){
+    /* API config */
     const options = {
         method: 'GET',
         headers: {
@@ -9,24 +10,29 @@ async function consulta(){
         }
     };
 
+    /* coleta pesquisa do input */
     let pesquisa = document.querySelector("#pesquisa").value;
     let resposta = await fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q='+pesquisa, options)
     .then(response => response.json())
     .catch(err => console.error(err));
     console.log(resposta);
 
+    /* coleta imagem da api */
     let imagemData = await fetch(resposta.data[0].album.cover_xl);
     let imagem = await imagemData.blob();
     const imageObjectURL = URL.createObjectURL(imagem);
     let img = document.createElement('img');
     img.setAttribute("class", "img");
+    img.setAttribute("alt", "album artwork");
     img.src = imageObjectURL;
     resultados.appendChild(img);
 
+    /* coleta dados do lançamento */
     let info = resposta.data[0].artist.name;
     info = info + " - " +resposta.data[0].album.title;
     document.querySelector(".infoTexto").innerHTML = info;
 
+    /* impede a repetição da mesma imagem */
     while(document.querySelectorAll('.img').length !== 1){
         document.querySelector(".img").remove();
     }
